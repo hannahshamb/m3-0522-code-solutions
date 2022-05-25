c5008cf9d5b4a599e57306b3373a3999a3cd3714
Hannah Engelhardt <hannahnengel@gmail.com>
2022-05-25 15:36:13 +0000

    Completed react-hot-button-component challenge

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HotButton from './hot-button';
+
+const button = <HotButton />;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(button);
