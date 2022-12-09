47d742af3730b2978611fc308467967e374467d0
Hannah Engelhardt <hannahnengel@gmail.com>
2022-12-09 22:30:43 +0000

    Completed postpone, take2nd, and takeSmaller

--- data-structures-queues/take-smaller.js
+++ data-structures-queues/take-smaller.js
@@ -1,5 +1,27 @@
 /* exported takeSmaller */
 
 function takeSmaller(queue) {
+  if (queue.peek() === undefined) {
+    return queue.peek();
+  }
 
+  let smallerValue;
+  const frontValue = queue.peek();
+  queue.dequeue();
+  const secondValue = queue.peek();
+
+  if (secondValue === undefined) {
+    smallerValue = frontValue;
+  } else {
+    queue.dequeue();
+    if (frontValue < secondValue) {
+      smallerValue = frontValue;
+      queue.enqueue(secondValue);
+    } else {
+      smallerValue = secondValue;
+      queue.enqueue(frontValue);
+    }
+  }
+
+  return smallerValue;
 }
