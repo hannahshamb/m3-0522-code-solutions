a730bb94ae3ae89445b7eb71353cc4d55bd8a003
Hannah Engelhardt <hannahnengel@gmail.com>
2022-12-10 00:15:29 +0000

    Completed data-structures-queues exercise

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,5 +1,29 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
+  let frontValue = queue.peek();
+  const count = index;
+  let indexValue;
 
+  if (frontValue === undefined) {
+    return frontValue;
+  }
+
+  for (let i = 0; i < count; i++) {
+    indexValue = queue.peek();
+    queue.dequeue();
+
+    if (queue.peek() === undefined) {
+      frontValue = indexValue;
+      break;
+    }
+    queue.enqueue(indexValue);
+  }
+
+  if (queue.peek() !== undefined) {
+    frontValue = queue.peek();
+  }
+  queue.dequeue();
+
+  return frontValue;
 }
