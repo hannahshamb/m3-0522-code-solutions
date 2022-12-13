8f26b7c4716955bb61729ceb3417bc9c9561e164
Hannah Engelhardt <hannahnengel@gmail.com>
2022-12-13 00:43:40 +0000

    Completed removeTail and includes

--- data-structures-linked-lists/remove-tail.js
+++ data-structures-linked-lists/remove-tail.js
@@ -1,5 +1,13 @@
 /* exported removeTail */
 
 function removeTail(list) {
+  const originalList = list.next;
 
+  while (list.next !== null) {
+    if (list.next.next === null) {
+      originalList.next.next = list.next.next;
+    }
+    list.next = list.next.next;
+  }
+  list.next = originalList;
 }
