ee23078c032a53e9237fdb027fc0f49adee53753
Hannah Engelhardt <hannahnengel@gmail.com>
2023-01-03 21:33:08 +0000

    Separated the previous, next, and specific index functionalities into separate methods

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -10,27 +10,15 @@ export default class Carousel extends React.Component {
     this.slideChange = this.slideChange.bind(this);
   }
 
-  slideChange(event) {
+  slideChange() {
     const { urls } = this.props;
     const { url } = this.state;
-
     let nextUrl;
 
     if (url !== undefined) {
       urls.forEach((item, index) => {
         if (item === url) {
-          if (event === undefined || event.target.getAttribute('action') === 'next') {
-            nextUrl = urls[index + 1];
-          } else if (event.target.getAttribute('action') === 'previous') {
-            if (index !== 0) {
-              nextUrl = urls[index - 1];
-            } else {
-              nextUrl = urls[urls.length - 1];
-            }
-          } else if (event.target.getAttribute('action') === 'target') {
-            const selectionIndex = event.target.getAttribute('selection');
-            nextUrl = urls[selectionIndex];
-          }
+          nextUrl = urls[index + 1];
         }
       });
     } else {
@@ -38,11 +26,57 @@ export default class Carousel extends React.Component {
     }
 
     this.setState({ url: nextUrl });
-    if (event !== undefined) {
-      const timeoutId = event.target.getAttribute('timeoutid');
-      clearTimeout(timeoutId);
+  }
+
+  nextImage(event) {
+    const { urls } = this.props;
+    const { url } = this.state;
+    let nextUrl;
+
+    if (url !== undefined) {
+      urls.forEach((item, index) => {
+        if (item === url) {
+          nextUrl = urls[index + 1];
+        }
+      });
     }
 
+    this.setState({ url: nextUrl });
+    const timeoutId = event.target.getAttribute('timeoutid');
+    clearTimeout(timeoutId);
+  }
+
+  previousImage(event) {
+    const { urls } = this.props;
+    const { url } = this.state;
+    let nextUrl;
+
+    if (url !== undefined) {
+      urls.forEach((item, index) => {
+        if (item === url) {
+          if (index !== 0) {
+            nextUrl = urls[index - 1];
+          } else {
+            nextUrl = urls[urls.length - 1];
+          }
+        }
+      });
+    }
+
+    this.setState({ url: nextUrl });
+    const timeoutId = event.target.getAttribute('timeoutid');
+    clearTimeout(timeoutId);
+  }
+
+  specificIndex(event) {
+    const { urls } = this.props;
+
+    const selectionIndex = event.target.getAttribute('selection');
+    const nextUrl = urls[selectionIndex];
+
+    this.setState({ url: nextUrl });
+    const timeoutId = event.target.getAttribute('timeoutid');
+    clearTimeout(timeoutId);
   }
 
   render() {
@@ -60,8 +94,8 @@ export default class Carousel extends React.Component {
     const circles = urls.map((item, index) => {
       let circle;
       if (item === url) {
-        circle = <i key={index} selection={index} onClick={this.slideChange} action='target' timeoutid={timeoutId} className="fa-solid fa-circle fa-2xl px-2"></i>;
-      } else circle = <i key={index} selection={index} onClick={this.slideChange} action='target' timeoutid={timeoutId} className="fa-regular fa-circle fa-2xl px-2"></i>;
+        circle = <i key={index} selection={index} onClick={this.specificIndex} timeoutid={timeoutId} className="fa-solid fa-circle fa-2xl px-2"></i>;
+      } else circle = <i key={index} selection={index} onClick={this.specificIndex} timeoutid={timeoutId} className="fa-regular fa-circle fa-2xl px-2"></i>;
       return circle;
     });
 
@@ -69,9 +103,9 @@ export default class Carousel extends React.Component {
       <div className="my-container">
         <div className='row w-100 mt-5'>
           <div className="col d-flex align-items-center justify-content-center">
-            <i onClick={this.slideChange} action='previous' timeoutid={timeoutId} className="fa-solid fa-chevron-left fa-2xl"></i>
+            <i onClick={this.previousImage} timeoutid={timeoutId} className="fa-solid fa-chevron-left fa-2xl"></i>
             <img className='mx-5 img' src={url} alt='pokemon'></img>
-            <i onClick={this.slideChange} action='next' timeoutid={timeoutId} className="fa-solid fa-chevron-right fa-2xl"></i>
+            <i onClick={this.nextImage} timeoutid={timeoutId} className="fa-solid fa-chevron-right fa-2xl"></i>
           </div>
         </div>
         <div className="row d-flex justify-content-center mt-5 w-100">
